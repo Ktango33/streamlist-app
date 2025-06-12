b539d65e5a4d18a851add80bcd659105b178a153
Kolton Tarango <ktango33@gmail.com>
2025-06-11 20:09:19 -0400

    Final commit for PWA project

--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
-// src/index.js
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import './index.css';
 import App from './App';
+import reportWebVitals from './reportWebVitals';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration'; // Note: this enables PWA support
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App /> {/* DO NOT wrap <App /> in <BrowserRouter> again here */}
+    <App />
   </React.StrictMode>
 );
+
+// Register the service worker to enable PWA features
+serviceWorkerRegistration.register();
